26f0e01d77191a3a04327ad7b88397792ef697eb
See3200 <meer3200@gmail.com>
2019-03-17 12:10:10 +0200

    Added navigation layout

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,3 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Drawer.css'
 
+
+const links = [1, 2, 3]
+
+class Drawer extends Component {
+
+    renderLinks() {
+        return links.map((link, index) => {
+            return (
+                <li key={index}>
+                    <a href="#" className={'linkItem'}>Link {link}</a>
+                </li>
+            )
+        })
+    }
+
+    render() {
+
+        const cls =['drawer']
+
+        if (!this.props.isOpen) {
+            cls.push('close')
+        }
+
+        return (
+            <nav className={cls.join(' ')}>
+                <ul>
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Drawer
